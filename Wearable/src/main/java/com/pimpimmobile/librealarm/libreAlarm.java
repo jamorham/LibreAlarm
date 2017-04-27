296e6e077134fe6b68982dce84abf6705ba55517
Jamorham <jamorham@users.noreply.github.com>
2017-04-27 13:57:40 +0100

    Wear: detect if device has nfc

--- Wearable/src/main/java/com/pimpimmobile/librealarm/libreAlarm.java
+++ Wearable/src/main/java/com/pimpimmobile/librealarm/libreAlarm.java
@@ -2,7 +2,10 @@ package com.pimpimmobile.librealarm;
 
 import android.app.Application;
 import android.content.Context;
+import android.nfc.NfcManager;
 import android.util.Log;
+
+import com.pimpimmobile.librealarm.shareddata.PreferencesUtil;
 
 
 /**
@@ -12,7 +15,7 @@ import android.util.Log;
 public class libreAlarm extends Application {
 
     private static final String TAG = "libreAlarm";
-
+    public static Boolean hasNFC;
     // private static Context context;
 
     @Override
@@ -20,6 +23,16 @@ public class libreAlarm extends Application {
         //   context = getApplicationContext();
         Log.d(TAG, "APPLICATION CREATED !!!");
         super.onCreate();
+        // detect if this device has nfc
+        final NfcManager nfcManager = (NfcManager) this.getSystemService(Context.NFC_SERVICE);
+        hasNFC = nfcManager.getDefaultAdapter() != null;
+        if (hasNFC && (PreferencesUtil.shouldUseRoot(getApplicationContext()))) {
+            RootTools.swichNFCState(false); // disable on start
+        }
+    }
+
+    public static boolean noNFC() {
+        return hasNFC != null && !hasNFC;
     }
 
     //   public static Context getAppContext() {
