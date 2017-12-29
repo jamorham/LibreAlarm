183d73da8209905f894f94328938b1638fc64e89
Tzachi Dar <tzachi.dar@gmail.com>
2017-12-29 12:36:36 +0200

    Read all 43 blocks and send them to xDrip.
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

--- shareddata/src/main/java/com/pimpimmobile/librealarm/shareddata/ReadingData.java
+++ shareddata/src/main/java/com/pimpimmobile/librealarm/shareddata/ReadingData.java
@@ -1,6 +1,7 @@
 package com.pimpimmobile.librealarm.shareddata;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ReadingData {
@@ -8,6 +9,7 @@ public class ReadingData {
     public PredictionData prediction;
     public List<GlucoseData> trend;
     public List<GlucoseData> history;
+    public byte[] raw_data;
 
     public ReadingData(PredictionData.Result result) {
         this.prediction = new PredictionData();
@@ -15,12 +17,16 @@ public class ReadingData {
         this.prediction.errorCode = result;
         this.trend = new ArrayList<>();
         this.history = new ArrayList<>();
+        // The two bytes are needed here since some components don't like a null pointer
+        this.raw_data = new byte[2];
     }
+    
 
-    public ReadingData(PredictionData prediction, List<GlucoseData> trend, List<GlucoseData> history) {
+    public ReadingData(PredictionData prediction, List<GlucoseData> trend, List<GlucoseData> history, byte[] data) {
         this.prediction = prediction;
         this.trend = trend;
         this.history = history;
+        this.raw_data = Arrays.copyOfRange(data, 0 ,0x158);
     }
 
     public ReadingData() {}
